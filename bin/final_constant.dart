92a382c514a4f306e982cbd70402e962e3d6b8ca
Avishekavi123 <97905931+Avishekavi123@users.noreply.github.com>
2022-06-08 14:11:18 +0600

    Dart Basics 2

---
+++ bin/final_constant.dart
@@ -0,0 +1,13 @@
+void main()
+{
+  //final
+  final int a=20; //Can assign a to b but no new number can be assign to a
+  int b=a;
+
+  //Constant
+
+  const int x=40; //this x cant be assigned toa any other variable
+   // int f=x //cant be done
+  //constant only have one value in one program
+
+}
